e33203b665921142398b381a3fe952c8840b36c5
MuxauL-cpu <neverno128@mail.ru>
2022-10-30 21:44:04 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,22 +7,12 @@ let profile__button = profile.querySelector('.profile__button');
 let UserName = profile.querySelector('.profile__name');
 let UserJob = profile.querySelector('.profile__job');
 
-//определение переменных для кнопки лайка
-let photo_grid = content.querySelector('.photo-grid');
-let photo_grid_list = photo_grid.querySelector('.photo-grid__list');
-let card = photo_grid_list.querySelector('.card');
-let button_like = card.querySelector('.card__button-like');
-
 //определение переменных pop-up, кнопки сохранения, закрыть. Переменные для изменения имени и работы пользователя
 let popup = document.querySelector('.popup');
 let button_submit = popup.querySelector('.popup__button-submit');
 let button_close = popup.querySelector('.popup__button-close');
 let nameInput = popup.querySelector('.popup__name-input');
 let jobInput = popup.querySelector('.popup__job-input');
-
-function btnLikeClick(evt) {
-    button_like.classList.toggle('card__button-like_enabled');
-}
 
 //при нажатии открывает форму и подаставляет имя пользователя и работу в поля ввода
 function formOpenHandler() {
