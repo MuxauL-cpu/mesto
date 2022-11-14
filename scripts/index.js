699fd3d944013d7048404a3c0fb85458e544b3f1
MuxauL-cpu <neverno128@mail.ru>
2022-11-14 13:25:59 +0300

    4.2 версия
    Переименовал названия для JavaScript по БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -7,21 +7,21 @@ const UserName = content.querySelector('.profile__name');
 const UserJob = content.querySelector('.profile__job');
 
 //определение переменных pop-up, кнопки сохранения, закрыть. Переменные для изменения имени и работы пользователя
-const popup = document.querySelector('.popup__user');
+const popup = document.querySelector('.popup__type_user');
 const popupForm = popup.querySelector('.popup__form');
 const buttonClose = popup.querySelector('.popup__button-close');
 const nameInput = popup.querySelector('.popup__input_type_name');
 const jobInput = popup.querySelector('.popup__input_type_job');
 
 //определение переменных для pop-up, который добавляет картинки
-const popupCards = document.querySelector('.popup__card-editor');
+const popupCards = document.querySelector('.popup__type_card-editor');
 const popupCardsForm = popupCards.querySelector('.popup__form_type_cards');
 const buttonCardsClose = popupCards.querySelector('.popup__button-close');
 const imageDescription = popupCards.querySelector('.popup__input_type_description');
 const imageLink = popupCards.querySelector('.popup__input_type_link');
 
 //определение переменных для popup, который открывает карточки
-const popupOpenImage = document.querySelector('.popup__open-image');
+const popupOpenImage = document.querySelector('.popup__type_open-image');
 const popupImage = popupOpenImage.querySelector('.popup__image');
 const popupCaption = popupOpenImage.querySelector('.popup__figcaption');
 const popupCloseImage = popupOpenImage.querySelector('.popup__button-close');
